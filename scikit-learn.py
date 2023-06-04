0790691e856d256478a04601e4da1b91926ad145
Belkacem <abel-and-co@hotmail.com>
2023-06-04 22:46:44 +0200

    Version initiale

--- scikit-learn.py
+++ scikit-learn.py
@@ -1,3 +1,7 @@
+# Import des Bibliothéques :
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import matplotlib.pyplot as plt
+# Données d'exemple : x et y sont deux vecteurs numpy de même taille
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([2.1, 4.2, 6.1, 8.0, 10.1])
